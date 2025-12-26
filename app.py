00b9008144b3d9991d1a93a8947fc73b56b68747
Luis Ruiz-Santiago <ldruizsan@gmail.com>
2025-12-25 22:54:28 -0500

    Updating columns

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ with open(resume_file,"rb") as pdf_file:
 profile_pic = Image.open(profile_pic)
 
 # HERO SECTION
-col1, col2 = st.columns(2, gap="small")
+col1, col2 = st.columns([1, 2], gap="small")
 with col1:
     st.image(profile_pic, width=350)
 
